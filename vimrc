20bd03a7fb9f0131eef33c3eed0612f4a3333362
Thomas Wood <grand.edgemaster@gmail.com>
2010-09-13 18:12:54 +0100

    Initial config imports

---
+++ vimrc
@@ -0,0 +1,10 @@
+filetype plugin indent on
+set expandtab
+set shiftwidth=2
+set softtabstop=2
+set background=dark
+syntax enable
+
+autocmd FileType php set omnifunc=phpcomplete#CompletePHP
+
+set ruler
---
+++ xmonad.hs
@@ -0,0 +1,28 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ withUrgencyHook NoUrgencyHook
+           $ defaultConfig
+      { manageHook = manageDocks <+> manageHook defaultConfig
+      , layoutHook = avoidStruts  $  layoutHook defaultConfig
+      , logHook    = dynamicLogWithPP $ sjanssenPP
+        { ppOutput = hPutStrLn xmproc
+        , ppOrder  = \(ws:_:t:_) -> [ws,t]
+        , ppUrgent = xmobarColor "#ee9a00" "" . xmobarStrip
+        }
+      , modMask = mod4Mask     -- Rebind Mod to the Windows key
+      } `additionalKeys`
+      [ ((mod4Mask, xK_z), spawn "xlock")
+      , ((mod4Mask, xK_n), spawn "exaile --next")
+      , ((mod4Mask, xK_b), spawn "exaile --play-pause")
+      , ((mod4Mask, xK_m), spawn "exaile --toggle-visible")
+      , ((mod4Mask, xK_p), spawn "dmenu_run -fn -misc-fixed-*-*-*-*-10-*-*-*-*-*-*-* -nb black -nf gray -sf '#ee9a00' -sb black")
+      ]
