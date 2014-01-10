a56fa1fb6f3f4d5162fcf732ce0ec832e4ee1d99
Thomas Wood <tw1509@doc.ic.ac.uk>
2014-01-10 11:07:53 +0000

    Relink, update xmonad.hs DoC config

--- xmonad.hs
+++ xmonad.hs
@@ -4,36 +4,42 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.NoBorders
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.MultiColumns
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Hooks.SetWMName
 
+orange = "#ee9a00"
+dmenu_config = " -fn -misc-fixed-*-*-*-*-10-*-*-*-*-*-*-* -nb black -nf gray -sf '" ++ orange ++ "' -sb black"
+myLayoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig ||| columns
+--columns = ThreeColMid 1 (3/100) (1/3)
+columns = multiCol [1,1,0] 4 0.01 0.5
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ withUrgencyHook NoUrgencyHook
            $ defaultConfig
       { manageHook = manageDocks <+> manageHook defaultConfig
-      , layoutHook = smartBorders $ avoidStruts  $  layoutHook defaultConfig
+      , layoutHook = myLayoutHook
       , logHook    = dynamicLogWithPP $ sjanssenPP
         { ppOutput = hPutStrLn xmproc
         , ppOrder  = \(ws:_:t:_) -> [ws,t]
-        , ppUrgent = xmobarColor "#ee9a00" "" . xmobarStrip
+        , ppTitle = xmobarColor orange "" . shorten 120
+        , ppUrgent = xmobarColor orange "" . xmobarStrip
         }
       , startupHook = setWMName "LG3D"
       , modMask = mod4Mask     -- Rebind Mod to the Windows key
-      , normalBorderColor = "#000000"
-      , focusedBorderColor = "#ee9a00"
+      , normalBorderColor = "#262626"
+      , focusedBorderColor = orange
+      , terminal = "xterm"
       } `additionalKeys`
-      [ ((mod4Mask, xK_z), spawn "slimlock")
---      , ((mod4Mask, xK_n), spawn "exaile --next --start-anyway")
---      , ((mod4Mask, xK_b), spawn "exaile --play-pause --start-anyway")
---      , ((mod4Mask, xK_m), spawn "exaile --toggle-visible --start-anyway")
+      [ ((mod4Mask, xK_z), spawn "xscreensaver-command -lock")
       , ((mod4Mask, xK_m), spawn "dmpc")
       , ((mod4Mask, xK_n), spawn "mpc next")
       , ((mod4Mask, xK_b), spawn "mpc toggle")
       , ((mod4Mask, xK_v), spawn "lastfm-love")
-      , ((mod4Mask, xK_p), spawn "dmenu_run -fn -*-*-*-*-*-10-*-*-*-*-*-*-* -nb black -nf gray -sf '#ee9a00' -sb black")
-      , ((0, 0x1008FF2F),  spawn "sudo pm-suspend")
+      , ((mod4Mask, xK_p), spawn ("dmenu_run" ++ dmenu_config))
+      , ((mod4Mask, xK_s), sendMessage ToggleStruts)
       ]
