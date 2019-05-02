7030de7a167dc1b305143284f557c172ffd95d46
Thomas Wood <grand.edgemaster@gmail.com>
2019-05-02 21:45:57 +0100

    Big updates

--- vimrc
+++ vimrc
@@ -9,11 +9,23 @@ set textwidth=100
 set colorcolumn=+1
 set background=dark
 
-" Colours / Filetypes
+" Colours
 colorscheme inkpot
 syntax enable
-filetype plugin indent on
-autocmd FileType php set omnifunc=phpcomplete#CompletePHP
+filetype plugin indent on   " Enable filetype indentation
+set foldlevelstart=99       " Starts with any folds open
+
+" Filetype-specific options and setup
+let g:tex_flavor='pdflatex'
+let g:xml_syntax_folding=1
+
+augroup filetypes
+  autocmd!
+  autocmd FileType php set omnifunc=phpcomplete#CompletePHP
+  autocmd BufRead,BufNewFile *.dokuwiki set filetype=dokuwiki
+  autocmd FileType xml setlocal foldmethod=syntax
+  autocmd FileType xml setlocal foldcolumn=1
+augroup END
 
 " Tab completion
 set wildignore=*.o,*.pyc,*.pdf,*.class,*.aux,*.hi
@@ -32,19 +44,23 @@ set guioptions=acimg
 set hlsearch
 set incsearch
 
-let g:tex_flavor='pdflatex'
 
 " Highlight trailing whitespace
 highlight ExtraWhitespace ctermbg=red guibg=red
 match ExtraWhitespace /\s\+$/
-autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
-autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
-autocmd InsertLeave * match ExtraWhitespace /\s\+$/
-autocmd BufWinLeave * call clearmatches()
+augroup trailing_ws
+  autocmd!
+  autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+  autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+  autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+  autocmd BufWinLeave * call clearmatches()
 
-" Fix trailing whitespace only on lines in range between entering and leaving insert mode
-autocmd InsertEnter * let b:insert_start = line('.')
-autocmd InsertLeave * call s:StripTrailingWhitespaces()
+  " Fix trailing whitespace only on lines in range between
+  " entering and leaving insert mode
+  autocmd InsertEnter * let b:insert_start = line('.')
+  autocmd InsertLeave * call s:StripTrailingWhitespaces()
+augroup END
+
 function! s:StripTrailingWhitespaces()
   let l:winview = winsaveview()
   exe b:insert_start . ',.s/\s\+$//e'
@@ -57,10 +73,12 @@ function! LoadTemplate()
   " Highlight %VAR% placeholders with the Todo colour group
   syn match Todo "%\u\+%" containedIn=ALL
 endfunction
-autocmd! BufNewFile * call LoadTemplate()
+augroup template
+  autocmd! BufNewFile * call LoadTemplate()
+augroup END
 
-augroup myvimrchooks
-  au!
+augroup vimrc
+  autocmd!
   autocmd bufwritepost .vimrc source ~/.vimrc | redraw | echo 'vimrc reloaded'
 augroup END
 
--- xmonad.hs
+++ xmonad.hs
@@ -1,27 +1,41 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.MultiColumns
+import qualified XMonad.StackSet as W
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
-import XMonad.Hooks.SetWMName
 
 orange = "#ee9a00"
 dmenu_config = " -fn -misc-fixed-*-*-*-*-10-*-*-*-*-*-*-* -nb black -nf gray -sf '" ++ orange ++ "' -sb black"
-myLayoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig ||| columns
+myLayoutHook = smartBorders $ avoidStruts $ layoutHook def ||| columns
 --columns = ThreeColMid 1 (3/100) (1/3)
 columns = multiCol [1,1,0] 4 0.01 0.5
+mask = mod4Mask
+
+myManageHook = composeAll
+  [ resource =? "x3270"       --> doFloat
+  , resource =? "keypadPopup" --> doIgnore
+  , className =? "stellaris" --> doFullFloat
+  , className =? "eu4" --> doFullFloat
+  , className =? "ck2" --> doFullFloat
+  , manageDocks
+  ]
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ withUrgencyHook NoUrgencyHook
-           $ defaultConfig
-      { manageHook = manageDocks <+> manageHook defaultConfig
+           $ ewmh
+           $ def
+      { manageHook = myManageHook <+> manageHook def
+      , handleEventHook = fullscreenEventHook <+> docksEventHook <+> handleEventHook def
       , layoutHook = myLayoutHook
       , logHook    = dynamicLogWithPP $ sjanssenPP
         { ppOutput = hPutStrLn xmproc
@@ -30,16 +44,27 @@ main = do
         , ppUrgent = xmobarColor orange "" . xmobarStrip
         }
       , startupHook = setWMName "LG3D"
-      , modMask = mod4Mask     -- Rebind Mod to the Windows key
+      , modMask = mask     -- Rebind Mod to the Windows key
       , normalBorderColor = "#262626"
       , focusedBorderColor = orange
-      , terminal = "xterm"
-      } `additionalKeys`
-      [ ((mod4Mask, xK_z), spawn "xscreensaver-command -lock")
-      , ((mod4Mask, xK_m), spawn "dmpc")
-      , ((mod4Mask, xK_n), spawn "mpc next")
-      , ((mod4Mask, xK_b), spawn "mpc toggle")
-      , ((mod4Mask, xK_v), spawn "lastfm-love")
-      , ((mod4Mask, xK_p), spawn ("dmenu_run" ++ dmenu_config))
-      , ((mod4Mask, xK_s), sendMessage ToggleStruts)
-      ]
+      , terminal = "konsole"
+      } `additionalKeys` myKeys
+
+myKeys = [ ((mask, xK_z), spawn "suspend.sh")
+         , ((mask, xK_m), spawn "dmpc")
+         , ((mask, xK_n), spawn "mpc next")
+         , ((mask, xK_b), spawn "mpc toggle")
+         , ((mask, xK_v), spawn "lastfm-love")
+         , ((mask, xK_p), spawn ("dmenu_run" ++ dmenu_config))
+         , ((mask, xK_s), sendMessage ToggleStruts)
+         ] ++
+         -- mod-[1..9] %! Switch to workspace N
+         -- mod-shift-[1..9] %! Move client to workspace N
+         [((m .|. mask, k), windows $ f i)
+           | (i, k) <- zip (XMonad.workspaces def) numPadKeys
+           , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+
+numPadKeys = [ xK_KP_End,  xK_KP_Down,  xK_KP_Page_Down -- 1, 2, 3
+             , xK_KP_Left, xK_KP_Begin, xK_KP_Right     -- 4, 5, 6
+             , xK_KP_Home, xK_KP_Up,    xK_KP_Page_Up   -- 7, 8, 9
+             ] --, xK_KP_Insert]                            -- 0
